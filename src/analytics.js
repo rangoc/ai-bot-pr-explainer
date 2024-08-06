f6c53bb3bb68adfcbbff4cc0b763d1dd0fb4d94a
rangoc <cabarkapa.goran@gmail.com>
2024-08-06 11:25:23 +0200

    add analytics.js

---
+++ src/analytics.js
@@ -0,0 +1,20 @@
+class Analytics {
+  constructor() {
+    this.events = [];
+  }
+
+  trackEvent(event) {
+    this.events.push(event);
+  }
+
+  getEventCount(eventType) {
+    return this.events.filter((e) => e.type === eventType).length;
+  }
+}
+
+const analytics = new Analytics();
+analytics.trackEvent({ type: "page_view", page: "home" });
+analytics.trackEvent({ type: "click", element: "buy_button" });
+analytics.trackEvent({ type: "page_view", page: "product" });
+console.log(analytics.getEventCount("page_view")); // Count of page views
+console.log(analytics.getEventCount("click")); // Count of clicks
